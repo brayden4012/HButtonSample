0ab8b14f031ebb95074194b6ca83b430967d5b36
Brayden Harris <bharris@tradestation.com>
2023-11-16 18:12:24 -0600

    Initial Commit

---
+++ HButtonSample/HButtonSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HButtonSampleApp.swift
+//  HButtonSample
+//
+//  Created by Brayden Harris on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct HButtonSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
